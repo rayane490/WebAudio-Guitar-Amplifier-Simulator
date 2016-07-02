7bb44789efe890f95b8a007dc7cc238b527de93e
EL MOUSLIH Mohamed Riyane <rayane490@gmail.com>
2016-07-02 04:26:35 +0200

    Basic CRUD for server.js

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 
+/* global __dirname */
+
 // We need to use the express framework: have a real web servler that knows how to send mime types etc.
 var express=require('express');
 
 // Init globals variables for each module required
 var app = express()
   , http = require('http')
-  , server = http.createServer(app)
+  , server = http.createServer(app);
 
 // launch the http server on given port
 server.listen(8082);
@@ -20,3 +22,16 @@ app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
 
+app.post('/', function (req, res) {
+  
+  res.send('POST request to homepage');
+  console.log("j'ai reçu cela");
+});
+
+app.put('/', function (req, res) {
+  res.send('PUT request to homepage');
+});
+
+app.delete('/', function (req, res) {
+  res.send('DELETE request to homepage');
+});
